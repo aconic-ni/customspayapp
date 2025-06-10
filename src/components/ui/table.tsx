e4f496e700b57071f808fea931f2c95d0a462ccc
aconic-ni <jerco27@gmail.com>
2025-06-09 23:51:27 -0600

    Update table.tsx

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -6,7 +6,7 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className="relative w-full overflow-auto">
+  <div className={cn("relative w-full overflow-auto max-h-[calc(100vh-380px)]")}> {/* Changed to overflow-auto and added max-h */}
     <table
       ref={ref}
       className={cn("w-full caption-bottom text-sm", className)}
