a037fc936065f18824931f927c647dbc4a2f63b6
aconic-ni <jerco27@gmail.com>
2025-07-15 17:24:49 -0600

    Update InitialInfoForm.tsx

--- src/components/examiner/InitialInfoForm.tsx
+++ src/components/examiner/InitialInfoForm.tsx
@@ -63,7 +63,7 @@ function onSubmit(data: InitialDataFormData) {
 }
 
   return (
-    <Card className="w-full max-w-3xl mx-auto custom-shadow">
+    <Card className="w-full custom-shadow">
       <CardHeader>
         <CardTitle className="text-2xl font-semibold text-gray-800">Solicitud de Cheque</CardTitle>
       </CardHeader>
@@ -176,7 +176,7 @@ function onSubmit(data: InitialDataFormData) {
                 name="ne"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>NE (Seguimiento NX1) * / No usar simbolos</FormLabel>
+                    <FormLabel>NE (Seguimiento NX1) * / No usar "/", ni "-".</FormLabel>
                     <FormControl>
                       <Input placeholder="Ej: NX112345" {...field} value={field.value ?? ''} />
                     </FormControl>
